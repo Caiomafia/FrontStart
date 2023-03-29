b034677b66b193adfce012ad4b0c7cad7be9c354
Caio Mafra <gamersb200@gmail.com>
2023-03-29 19:44:57 -0300

    Aulas

---
+++ 02.tiposDeDados/TiposDados.js
@@ -0,0 +1,24 @@
+//Tipos de dados no js 
+
+
+
+/* 
+Tipo Boleano (verdadeiro ou falso )
+Tipo undefield (não definido)
+Tipo Number(number)
+Tipo string (letras)
+Tipo objeto 
+Tipo Null (não existe) 
+
+
+
+
+
+
+
+
+
+
+
+
+*/
---
+++ 03.JSteoria/READMEN.md
@@ -0,0 +1,1 @@
+https://github.com/isadorastan/estudos/blob/master/README.md
