2e3273544204d6b80952821ea2ca35ef2c7d502b
Caio Mafra <gamersb200@gmail.com>
2023-03-29 19:12:51 -0300

    Aulas

--- 01/01.introdução.js
+++ 01/01.introdução.js
@@ -1,3 +1,19 @@
 // var > não se usa mais 
 //let > pode mudar o valor 
 // const > e um calor constante , não muda 
+
+
+
+
+ let soma = 0 ; 
+ let somaEl = document.getElementById('somaEl');
+
+ somaEl.innerHTML = soma ;
+
+ function somar(){
+    console.log('somando');
+    somaEl.innerHTML = soma ; 
+ }
+
+
+
