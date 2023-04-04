3bbea88d228a1349f1966c5261d9ccce61a82c79
Caio Mafra <gamersb200@gmail.com>
2023-04-04 19:22:22 -0300

    Aulas

--- JS/08.atividades/atividade.js
+++ JS/08.atividades/atividade.js
@@ -1,8 +1,7 @@
 
-
-
-
-
+const serviceText = document.querySelector(".serviceText") ;
+const contactText = document.querySelector(".contactText");
+const infoText = document.querySelector(".Infotex");
 
 const  items = Array.from(document.getElementsByClassName('item'));
 
@@ -16,20 +15,39 @@ items.forEach(item => {
 
 function onItemClick(event){
     const selectedAttribute = event.target.getAttribute('data');
+    addOrRemove(selectedAttribute);
+    hideOrShowTextdiv(selectedAttribute);
+}
+
+function hideOrShowTextdiv(selectedAttribute){
+if(selectedAttribute === 'contact') {
+    contactText.style.display = 'block';
+}
+
+if(selectedAttribute === 'service') {
+    serviceText.style.display = 'block';
+}
+
+}
 
 
+function addOrRemove(selectedAttribute){
+    items.forEach(item => {
+        const itemAttribute  = item.getAttribute('data');
+        if(itemAttribute  == selectedAttribute){
+         item.classList.add('active');
+         return;
+        }
+        
+         item.classList.remove('active')
+        
+     
+     });
+}
 
 
-items.forEach(item => {
-   const itemAttribute  = item.getAttribute('data');
-   if(itemAttribute  == selectedAttribute){
-    item.classList.add('active');
-    return;
-   }
-   
-    item.classList.remove('active')
-   
-
-});
-
+ function hideTextElements (){
+    serviceText.style.display ='none';
+    infoText.style.display ='none';
+    contactText.style.display ='none';
 }
