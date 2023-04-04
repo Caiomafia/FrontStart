caf410c701efa0bed7ab72da4976fc2ade35b8ea
Caio Mafra <gamersb200@gmail.com>
2023-04-04 17:48:37 -0300

    Aulas

--- JS/08.atividades/atividade.js
+++ JS/08.atividades/atividade.js
@@ -1,1 +1,35 @@
-0
+
+
+
+
+
+
+const  items = Array.from(document.getElementsByClassName('item'));
+
+
+
+items.forEach(item => {
+    item.addEventListener('click' , onItemClick);
+
+})
+
+
+function onItemClick(event){
+    const selectedAttribute = event.target.getAttribute('data');
+
+
+
+
+items.forEach(item => {
+   const itemAttribute  = item.getAttribute('data');
+   if(itemAttribute  == selectedAttribute){
+    item.classList.add('active');
+    return;
+   }
+   
+    item.classList.remove('active')
+   
+
+});
+
+}
