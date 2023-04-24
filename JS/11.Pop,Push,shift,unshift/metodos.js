26da41850c344435e237a928d4b4cfce87aea45f
Caio Mafra <gamersb200@gmail.com>
2023-04-24 17:27:32 -0300

    Atualização

---
+++ JS/11.Pop,Push,shift,unshift/metodos.js
@@ -0,0 +1,16 @@
+/*
+Push : add no iultimo index do array 
+Pop: remove o ultimo do index
+
+unshift : add em 1 lugar no array
+shift : remove em 1 lugar do array 
+
+
+*/ 
+
+let names = ["Caio","Mafra"]
+console.log(names.pop())
+
+names.push("Anne" , "Glebson" , "Alexanfria ")
+ 
+console.log(names.unshift())
