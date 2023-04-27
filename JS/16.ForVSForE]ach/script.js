4971ac6fdcc58a0041c7ca832e6a190a87d4adf5
Caio Mafra <gamersb200@gmail.com>
2023-04-27 20:19:30 -0300

    Atualização

---
+++ JS/16.ForVSForE]ach/script.js
@@ -0,0 +1,18 @@
+// Ambos são para repetição ou interação 
+
+// Para fazer a atividade 
+
+const number = [1,2,3,4,5,6]
+
+
+
+//For 
+for (let i = 0; i < number.length ; i++) {
+    console.log(number[i])
+    
+}
+
+
+//Foreach
+
+number.forEach((value,index,array) => console.log(index))
