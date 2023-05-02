ea3dd354f10d6b4c5eb296924ed7b91389a617f1
Caio Mafra <gamersb200@gmail.com>
2023-05-02 17:11:22 -0300

    Atualização

--- JS/20.Pomodoro/script.js
+++ JS/20.Pomodoro/script.js
@@ -4,15 +4,15 @@ let TIME_IN_SECONDS = INICIAR_TIME_IN_SECONDS;
 
 setInterval(() => {
 
-INICIAR_TIME_IN_SECONDS--;
+TIME_IN_SECONDS--;
 
 const minute = Math.floor(TIME_IN_SECONDS / 60)
 
-document.getElementById('min').innerHTML = minute
+document.getElementById('min').innerHTML = minute;
 
 
-const seconds = 
-
+const seconds = (TIME_IN_SECONDS % 60) < 10; 
+document.getElementById('sec').innerHTML = seconds;
 
 
 } , 1000)
