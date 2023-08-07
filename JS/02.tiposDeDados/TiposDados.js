e375d2e880ec8584d004f9b5012677f622eb7fd3
Caio Mafra <gamersb200@gmail.com>
2023-08-07 20:51:15 -0300

    Aula 115

--- JS/02.tiposDeDados/TiposDados.js
+++ JS/02.tiposDeDados/TiposDados.js
@@ -22,3 +22,36 @@ Tipo Null (não existe)
 
 
 */
+
+
+
+
+let name = "caio";
+
+///strint
+
+
+let channel = {
+  name:'caio',
+  age:78,
+  stars:11.50 ,
+  isSubscribed:true,
+}
+
+console.log{`${channel.name}  e minha idade  e de ${channel.age} e tenho ${channel.stars} de estrelas `
+}
+
+
+let arrayvazio = [ ];
+
+
+channel = [studetes];
+
+const Mafra = {
+    name:"Mafra",
+    age:18,
+    weight:1.80,
+}
+
+
+channel[1] = Mafra
---
+++ JS/03.aFunction/function.js
@@ -0,0 +1,32 @@
+/*Aqui vamos tratar da function muito usada no dia a dia */
+
+
+
+function namex(parametro){ação}
+
+
+/*declaration - função de declaração
+
+--agrupamentos dos codigos 
+--reutilização
+--organizado 
+*/
+function ola(){
+    console.log('Ola')
+}
+
+ola() //sempre chamar ela de volta
+console.log('tchau ');
+
+
+
+
+function chamar(){
+
+    function mensagem(){
+        return 'Óla'
+    }
+
+    console.log(mensagem)
+
+}
