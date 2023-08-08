afce9af7df0f2da57e5d2306c968ab972e05bdfb
Caio Mafra <gamersb200@gmail.com>
2023-08-07 22:06:18 -0300

    Commit aula 138

--- JS/03.aFunction/function.js
+++ JS/03.aFunction/function.js
@@ -1,32 +1,60 @@
 /*Aqui vamos tratar da function muito usada no dia a dia */
 
+// uma parte muito importante chamada function muito usada no dia a dia , praticamente 
+//isso server para executar essa função varías e varías vezes . 
 
 
-function namex(parametro){ação}
 
-
-/*declaration - função de declaração
-
---agrupamentos dos codigos 
---reutilização
---organizado 
-*/
-function ola(){
-    console.log('Ola')
+function somar (x , y){
+    console.log(x + y);
 }
 
-ola() //sempre chamar ela de volta
-console.log('tchau ');
+
+somar(50 , 60 ) // retorno de 110
+
+
+//[usando agora function anonimas ou function escondidas ]
+
+const juntar - function(nomex , sobrenome){ console.log(` ${nomex} é ${sobrenome}`)}
+
+
+juntar("caio", 'mafra') //result [ Caio é Mafra]
 
 
 
+//             {Mais sobre functions }
 
-function chamar(){
 
-    function mensagem(){
-        return 'Óla'
-    }
+const sum - function(numero1,numero2){
+    let total - numero1 + numero2;
+    //quando e declarado return elá finaliza de devolve o que foi capturado 
 
-    console.log(mensagem)
-
+    return total  //  (nesse caso vai devolver a somatorio dos valores)
 }
+
+
+
+
+sum(10,50)
+
+
+
+// Função liquidificador 
+
+
+const liquidificador - function(fruta1 , fruta2){
+    return fruta1 + fruta2
+}
+
+
+const copo - liquidificador('maça' , 'banana');
+
+
+console.log(copo)//suco
+
+
+
+
+//function scope
+
+let subject
