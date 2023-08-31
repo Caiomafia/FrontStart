0f4a46a237e8e8374a189058f5e01e8691e3017e
Caio Mafra <gamersb200@gmail.com>
2023-08-31 20:30:37 -0300

    Estrutura de repetição

---
+++ EstruturasRe/for.js
@@ -0,0 +1,16 @@
+//estrutura de repitição 
+
+
+
+//for 
+
+
+for(let i = 0 ; i <=50 ; i++)
+{
+
+    if(i === 5){
+        console.log(numero impar)
+    }
+console.log(i)
+
+}
---
+++ EstruturasRe/forIn.js
@@ -0,0 +1,12 @@
+// for in 
+
+let person = {
+    name:'Caio',
+    age:19,
+    weight:90
+}
+for( let leitura in person){
+
+    
+   console.log(`os dados ${leitura}:   ${person[leitura]}`) 
+}
