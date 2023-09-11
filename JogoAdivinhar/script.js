b7e23573c85aef5da27f2470d44392544bf2c8bb
Caio Mafra <gamersb200@gmail.com>
2023-09-11 07:35:20 -0300

    Aula 160

--- JogoAdivinhar/script.js
+++ JogoAdivinhar/script.js
@@ -1,4 +1,23 @@
 function tentar(event){
     event.preventDefault()
- console.log(event)
+ 
 }
+
+const inputT = document.querySelector('#inputT')
+
+console.log(inputT.value)
+
+
+
+let tentativasx = document.querySelector('x');
+
+
+tentativasx.innerHTML = contagem.
+
+
+let randonnumber = Math.round(Math.random * 10);
+
+let xxtentativas = 1 ; 
+
+
+
