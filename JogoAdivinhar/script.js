563c439e6d4cd36c21643eda0443fb442a01efd9
Caio Mafra <gamersb200@gmail.com>
2023-09-11 07:40:29 -0300

    Teste

--- JogoAdivinhar/script.js
+++ JogoAdivinhar/script.js
@@ -8,16 +8,3 @@ const inputT = document.querySelector('#inputT')
 console.log(inputT.value)
 
 
-
-let tentativasx = document.querySelector('x');
-
-
-tentativasx.innerHTML = contagem.
-
-
-let randonnumber = Math.round(Math.random * 10);
-
-let xxtentativas = 1 ; 
-
-
-
