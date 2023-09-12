8e0775fe5e3b8b493f89c1c12ee113b78bd3d302
Caio Mafra <gamersb200@gmail.com>
2023-09-12 20:34:53 -0300

    Commit True

--- JogoAdivinhar/script.js
+++ JogoAdivinhar/script.js
@@ -1,6 +1,11 @@
-const randomNumber = Math. round (Math.random() * 10)
+const randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1 ; 
 
+alert(randomNumber);
+
+
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
 
 
 function Tentarbtn(event) {
@@ -9,11 +14,14 @@ event.preventDefault();
 const inputNumber = document.querySelector("#inputNumber")
 
 if (Number(inputNumber.value) == randomNumber){  
-document.querySelector(".screen1").classList.add("hide")
- document.querySelector(".screen2").classList.remove("hide")
+    screen1.classList.add("hide")
+    screen2.classList.remove("hide")
 
-document.querySelector(".screen2 h2").innerText = `Você acertou em ${xAttempts}`
+document.querySelector(".screen2 h2").innerText = "Você acertou em "+xAttempts + " top dms ";
 
 }
 xAttempts++
 }
+
+
+
