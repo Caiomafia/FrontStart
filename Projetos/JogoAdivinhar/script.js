9b7d46ba2f40c41de31c0c549304bbdcae026fa6
Caio Mafra <91575842+Caiomafia@users.noreply.github.com>
2024-01-09 18:19:17 -0300

    Aula nova

--- Projetos/JogoAdivinhar/script.js
+++ Projetos/JogoAdivinhar/script.js
@@ -1,26 +1,23 @@
 const randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1 ; 
 
-alert(randomNumber)
+alert(`${randomNumber} esse e o da vez`)
 
 
 const screen1 = document.querySelector(".screen1");
 const screen2 = document.querySelector(".screen2");
 
 
-function Tentarbtn(event) {
-event.preventDefault();
 
+function Tentarbtn(event) {
+event.preventDefault()
 const inputNumber = document.querySelector("#inputNumber")
 
-if (Number(inputNumber.value) === randomNumber){  
-    screen1.classList.add("hide")
-    screen2.classList.remove("hide")
+if(Number(inputNumber) == randomNumber){
 
-document.querySelector(".screen2 h2").innerText = "Você acertou em "+xAttempts + " top dms ";
-
+  alert("e igual")
 }
-inputNumber.innerText = "";
+
 xAttempts++
 }
 
