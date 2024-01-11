f8f2fb6c48d1cc4a34ce2fcbc515a6fbfe0e3b7d
Caio Mafra <91575842+Caiomafia@users.noreply.github.com>
2024-01-11 19:20:50 -0300

    Desafio 01

--- Projetos/JogoAdivinhar/script.js
+++ Projetos/JogoAdivinhar/script.js
@@ -1,45 +1,41 @@
-const randomNumber = Math.round(Math.random() * 10)
-let xAttempts = 1 ; 
+// variaveis
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
 
-alert(`${randomNumber} esse e o da vez`)
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+
+let randomNumber = Math.round(Math.random() * 10)
+let xAttempts = 1;
 
 
-const screen1 = document.querySelector(".screen1");
-const screen2 = document.querySelector(".screen2");
+//Eventos
+btnTry.addEventListener('click', handleClick)
+btnReset.addEventListener('click',handleResetClick)
 
 
-
-function Tentarbtn(event) {
-event.preventDefault()
-const inputNumber = document.querySelector("#inputNumber")
-
-if(Number(inputNumber) == randomNumber){
-
-  alert("e igual")
+//Funções
+function handleClick(event) {
+  event.preventDefault() //Não fazer o que é de origem.
+  const inputNumber = document.querySelector("#inputNumber")
+  
+if(Number(inputNumber.value) == randomNumber){
+  toggleScreen()
+  screen2.querySelector("h2").innerText = `Acertou em ${xAttempts} tentativas.`
 }
-
+inputNumber.value = ""
 xAttempts++
 }
 
+function handleResetClick(){
+  toggleScreen()
+  randomNumber = Math.round(Math.random() * 10)
+  xAttempts = 1
+}
 
+function toggleScreen(){
+  screen1.classList.toggle("hide")
+  screen2.classList.toggle("hide")
+}
 
-//Eventos 
-
-
-const novoBtn = document.querySelector(".Novobtn");
-const reset = document.querySelector("#reset");
-
-
-//call back
-reset,
-
-
-novoBtn.addEventListener('click' , function(event){
-
-    event.preventDefault
-    screen1.classList.remove("hide")
-    screen2.classList.add("hide")
-  inputNumber.value = ""
-  
-
-})
+alert(randomNumber)
