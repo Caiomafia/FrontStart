d8612f716b579a77c63e9b810f3b1e53bb0e5217
Caio Mafra <91575842+Caiomafia@users.noreply.github.com>
2024-01-24 14:34:38 -0300

    commit ajuste

---
+++ Projetos/IMC/main.js
@@ -0,0 +1,33 @@
+
+const height = document.querySelector("#height")
+const weight = document.querySelector("#weight")
+
+
+
+
+
+
+
+
+
+function calcular  (event) {
+    event.preventDefault();
+   
+    let soma = weight.value / (height.value * height.value)
+  
+ console.log(soma.value);
+    
+
+
+
+
+}
+
+
+
+imc = (height , weight ) =>{
+    height;
+    weight;
+    
+    return soma
+}
