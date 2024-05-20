b74f75c624cbc5d1b6b81432f66d1c3d6b9a488c
Caio Mafra <91575842+Caiomafia@users.noreply.github.com>
2024-05-20 11:13:30 -0300

    introdução ao curso

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,52 @@
 import './App.css'
 
 function App() {
- 
-const luana = {
-  
-}
+
+  const luana = {
+    cliente: 'LuN',
+    idade: 31,
+    compras: [
+      { nome: 'Notebook', preco: 'R$ 2500' },
+      { nome: 'Geladeira', preco: 'R$ 3000' },
+      { nome: 'Smartphone', preco: 'R$ 1500' },
+      { nome: 'Guitarra', preco: 'R$ 3500' },
+    ],
+    ativa: false,
+  };
+
+const mario = {
+  cliente: 'Mario',
+  idade: 31,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+    { nome: 'Guitarra', preco: 'R$ 3500' },
+  ],
+  ativa: false,
+};
 
 
-
-
+const dados = luana
+const total = dados.compras
+    .map((item) => Number(item.preco.replace('R$ ', '')))
+    .reduce((a, b) => a + b);
 
   return (
     <>
       
       <div>
-        <h2>Teste de projeto</h2>
+        <h2>Nome : {dados.cliente}</h2>
+        <p>Idade : {dados.idade}</p>
+        <p>
+          Situação : {' '}
+          <span style={{color:dados.ativa ? 'green' : 'red'}}>
+            {dados.ativa ? 'ativa ' : 'inativa'}
+          </span>
+        </p>
       </div>
-       
+       <p> Total : {total}</p>
+       {total > 10000 && <p>Você está gastando muito</p>}
     </>
   )
 }
